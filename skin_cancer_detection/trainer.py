578af2681aa9926efe4bc0b3a3012370edcf353e
Sabrina Daun <sabrinadaun@gmx.de>
2022-06-02 16:21:37 +0200

    incomplete get data from gcp function

---
+++ skin_cancer_detection/trainer.py
@@ -0,0 +1,17 @@
+from skin_cancer_detection.data import get_data_from_gcp
+
+if __name__ == "__main__":
+    # Get and clean data
+    N = 100
+    df = get_data_from_gcp(nrows=N)
+
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Train and save model, locally and
+    trainer = Trainer(X=X_train, y=y_train)
+    trainer.set_experiment_name('xp2')
+    trainer.run()
+    rmse = trainer.evaluate(X_test, y_test)
+    print(f"rmse: {rmse}")
+    trainer.save_model()
